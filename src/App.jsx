c60e8deab06529cf0a0e2b0397562682519c77c6
Razmik <karine@MacBook-Air-Karine.local>
2025-11-14 20:22:32 +0400

    trying to add business logic, using reducer and context

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, createContext } from "react";
 import "./App.css";
 import Input from "./components/Input/Input";
 import Todos from "./components/Todos/Todos";
+
+let MyContext = createContext(null);
 
 function App() {
   const textstate = useState("");
   const todosstate = useState([]);
 
-
- 
-
   return (
     <div className="parent-todo">
       <h1>
-        Write your tasks here or be 
+        Write your tasks here or be
         <br />
-        condemned forever you little pumpkin
-        head🎃🎃🎃
+        condemned forever you little pumpkin head🎃🎃🎃
       </h1>
-      <Input textstate={textstate} todosstate={todosstate}/>
-      <Todos todosstate={todosstate}/>
+      <Input textstate={textstate} todosstate={todosstate} />
+      <Todos todosstate={todosstate} />
     </div>
   );
 }
