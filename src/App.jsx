80e31d2d94d23afb8ac3ed50011ed5df51cbf802
Razmik <karine@MacBook-Air-Karine.local>
2025-11-14 17:54:18 +0400

    added components folder with respective 'Input' and 'Todos' folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import React,{ useState } from "react";
 import "./App.css";
 
 function App() {
+
+  
   return (
     <>
       <h1>Hello Razmik, the true prince and master of web development</h1>
